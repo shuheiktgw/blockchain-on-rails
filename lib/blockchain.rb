f49ab7165ff73f4fa5ff21bedd4fcd13e8d9e4e9
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-11 08:39:23 +0900

    scafofolded blockchain class

---
+++ lib/blockchain.rb
@@ -0,0 +1,19 @@
+class Blockchain
+
+  def initialize
+    @chain = []
+    @current_transactions = []
+  end
+
+  def create_block
+  end
+
+  def create_transaction
+  end
+
+  def hash(_block)
+  end
+
+  def last_block
+  end
+end
