aa71c947a7adaa275d80fb6ac4e8313ec993a8fe
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-11 09:03:59 +0900

    implement append_transaction method

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -1,19 +1,33 @@
 class Blockchain
 
+  attr_accessor :chain, :transactions
+
   def initialize
     @chain = []
-    @current_transactions = []
+    @transactions = []
   end
 
   def create_block
   end
 
-  def create_transaction
+  # Creates a new transaction to go into the next mined Block
+  # @param  sender: <str> Address of the Sender
+  # @param  recipient: <str> Address of the Recipient
+  # @param  amount: <int> Amount
+  # @return <int> The index of the Block that will hold this transaction
+  def append_transaction(sender:, recipient:, amount:)
+    transactions.push(Transaction.new(sender, recipient, amount))
+    last_block.index
   end
 
   def hash(_block)
   end
 
   def last_block
+    chain[-1]
   end
+
+  private
+
+  Transaction = Struct.new(:sender, :recipient, :amount)
 end
