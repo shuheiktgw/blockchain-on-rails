1790c7b8ccd6fdfe30000f8dbf659d791b1820b4
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-11 09:12:06 +0900

    defined create_transaction method

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -7,16 +7,24 @@ class Blockchain
     @transactions = []
   end
 
-  def create_block
+  # Create a new Block in the Blockchain
+  # @param proof: <int> The proof given by the Proof of Work algorithm
+  # @param previous_hash: (Optional) <str> Hash of previous Block
+  # @return <Block> New Block
+  def create_block(proof:, previous_hash: nil)
+
   end
 
   # Creates a new transaction to go into the next mined Block
+  #
   # @param  sender: <str> Address of the Sender
   # @param  recipient: <str> Address of the Recipient
   # @param  amount: <int> Amount
   # @return <int> The index of the Block that will hold this transaction
   def append_transaction(sender:, recipient:, amount:)
-    transactions.push(Transaction.new(sender, recipient, amount))
+    tr = create_transaction(sender: sender, recipient: recipient, amount: amount)
+    transactions.push(tr)
+
     last_block.index
   end
 
@@ -30,4 +38,8 @@ class Blockchain
   private
 
   Transaction = Struct.new(:sender, :recipient, :amount)
+
+  def create_transaction(sender:, recipient:, amount:)
+    Transaction.new(sender, recipient, amount)
+  end
 end
