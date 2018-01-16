fa0764667e0fb626b77f1289efdef05ab97bbe20
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-16 19:58:23 +0900

    create blocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180116105100) do
+
+  create_table "blocks", force: :cascade do |t|
+    t.integer "proof", null: false
+    t.string "previous_hash"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
