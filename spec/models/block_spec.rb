dadd5d618ebd54f15f38e7f3a0ffe2c28fb412f2
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-16 20:31:44 +0900

    add a test case for block#new

--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Block, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#new' do
+    it 'does not raise any errors' do
+      expect { Block.new(proof: 100, previous_hash: 1) }.to_not raise_error
+    end
+  end
+
+
 end
