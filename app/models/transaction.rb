d4ff2d939f05f3cb1614d9cd63013865e80e8621
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-16 20:40:21 +0900

    implement #Transaction.append_transaction!

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,18 @@
 class Transaction < ApplicationRecord
   belongs_to :block
 
+  # Creates a new transaction to go into the next mined Block
+  #
+  # @param  sender: <str> Address of the Sender
+  # @param  recipient: <str> Address of the Recipient
+  # @param  amount: <int> Amount
+  # @return <int> The index of the Block that will hold this transaction
+  def self.append_transaction!(sender:, recipient:, amount:)
+    block = Block.last
+    block.transactions.create!(sender: sender, recipient: recipient, amount: amount)
+    block.id
+  end
+
+
 
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  describe '#self.append_transaction' do
-    subject { Transaction.last }
+  before do
+    Block.create(proof: 100, previous_hash: 1)
+  end
 
-    before do
-      Transaction.append_transaction!(sender: 'test_sender', recipient: 'test_recipient', amount: 100)
+  describe '#self.append_transaction' do
+    subject(:id) { Transaction.append_transaction!(sender: 'test_sender', recipient: 'test_recipient', amount: 100) }
+    subject(:transaction) { Transaction.last }
+
+    it "returns block's id" do
+      expect(id).to eq 1
     end
 
     it 'appends transaction to the last block' do
+      id
       expect(subject.block_id).to eq 1
       expect(subject.sender).to eq 'test_sender'
     end
