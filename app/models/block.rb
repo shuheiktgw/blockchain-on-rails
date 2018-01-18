d1c6ff5ecb678d2a159c86bdce112eafc0843136
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-18 19:25:42 +0900

    add active model seralizer to gemfile

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,4 +1,13 @@
 class Block < ApplicationRecord
   has_many :transactions
 
+  # Create a new Block in the Blockchain
+  #
+  # @param proof: <int> The proof given by the Proof of Work algorithm
+  # @param previous_hash: (Optional) <str> Hash of previous Block
+  # @return <Block> New Block
+  def self.append_block(proof:, previous_hash: nil)
+
+    # self.create!(proof: proof, previous_hash: previous_hash || Blockchain.compute_)
+  end
 end
--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -15,34 +15,6 @@ class Blockchain
     append_block(proof: 100, previous_hash: 1)
   end
 
-  # Create a new Block in the Blockchain
-  #
-  # @param proof: <int> The proof given by the Proof of Work algorithm
-  # @param previous_hash: (Optional) <str> Hash of previous Block
-  # @return <Block> New Block
-  def append_block(proof:, previous_hash: nil)
-    block = create_block(proof: proof, previous_hash: previous_hash)
-    chain.push(block)
-
-    # reset the current list of transactions
-    @current_transactions = []
-
-    block
-  end
-
-  # Creates a new transaction to go into the next mined Block
-  #
-  # @param  sender: <str> Address of the Sender
-  # @param  recipient: <str> Address of the Recipient
-  # @param  amount: <int> Amount
-  # @return <int> The index of the Block that will hold this transaction
-  def append_transaction(sender:, recipient:, amount:)
-    transaction = create_transaction(sender: sender, recipient: recipient, amount: amount)
-    current_transactions.push(transaction)
-
-    last_block.index
-  end
-
   # Simple Proof of Work Algorithm:
   # Find a number p' such that compute_hash(pp') contains leading 4 zeroes, where p is the previous p'
   # p is the previous proof, and p' is the new proof
@@ -56,28 +28,6 @@ class Blockchain
     end
 
     proof
-  end
-
-  def last_block
-    chain[-1]
-  end
-
-  private
-
-  Transaction = Struct.new(:sender, :recipient, :amount) do
-    def to_s
-      "{ sender: #{ sender.to_s }, recipient: #{ recipient.to_s }, amount: #{ amount.to_s } }"
-    end
-  end
-
-  Block = Struct.new(:index, :timestamp, :current_transactions, :proof, :previous_hash) do
-    def to_s
-      "{ index: #{ index.to_s }, timestamp: #{ timestamp.to_s }, current_transactions: #{ current_transactions.map(&:to_s).to_s }, proof: #{ proof.to_s }, previous_hash: #{ previous_hash.to_s } }"
-    end
-  end
-
-  def create_transaction(sender:, recipient:, amount:)
-    Transaction.new(sender, recipient, amount)
   end
 
   def create_block(proof:, previous_hash:)
