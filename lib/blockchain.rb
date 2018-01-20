70341b5c9b5d4683f3dbc5f4fc7a118b32e6084b
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-20 13:17:02 +0900

    updated blockchain library

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -15,48 +15,32 @@ class Blockchain
     def compute_hash(obj)
       Digest::SHA256.hexdigest(obj.to_s)
     end
-  end
 
-  def initialize
-    @chain = []
-    @current_transactions = []
+    # Simple Proof of Work Algorithm:
+    # Find a number p' such that compute_hash(pp') contains leading 4 zeroes, where p is the previous p'
+    # p is the previous proof, and p' is the new proof
+    #
+    # @param last_proof: <int>
+    # @return: <int>
+    def compute_proof_of_work(last_proof)
+      proof = 0
+      until valid_proof?(last_proof: last_proof, proof: proof)
+        proof += 1
+      end
 
-    # Create the genesis block
-    append_block(proof: 100, previous_hash: 1)
-  end
-
-  # Simple Proof of Work Algorithm:
-  # Find a number p' such that compute_hash(pp') contains leading 4 zeroes, where p is the previous p'
-  # p is the previous proof, and p' is the new proof
-  #
-  # @param last_proof: <int>
-  # @return: <int>
-  def compute_proof_of_work(last_proof)
-    proof = 0
-    until valid_proof?(last_proof: last_proof, proof: proof)
-      proof += 1
+      proof
     end
 
-    proof
-  end
+    private
 
-  def create_block(proof:, previous_hash:)
-    Block.new(
-      chain.length,
-      Time.now.to_i,
-      current_transactions,
-      proof,
-      previous_hash || compute_hash(last_block)
-    )
-  end
-
-  # Validates the Proof: Does compute_hash(last_proof, proof) contain 4 leading zeroes?
-  #
-  # @param last_proof: <int> Previous Proof
-  # @param proof:      <int> Current Proof
-  # @return: <bool> True if correct, False if not.
-  def valid_proof?(last_proof:, proof:)
-    guess_hash = compute_hash(last_proof.to_s + proof.to_s)
-    guess_hash[-4..-1] == CORRECT_LAST_4_DIGITS
+    # Validates the Proof: Does compute_hash(last_proof, proof) contain 4 leading zeroes?
+    #
+    # @param last_proof: <int> Previous Proof
+    # @param proof:      <int> Current Proof
+    # @return: <bool> True if correct, False if not.
+    def valid_proof?(last_proof:, proof:)
+      guess_hash = compute_hash(last_proof.to_s + proof.to_s)
+      guess_hash[-4..-1] == CORRECT_LAST_4_DIGITS
+    end
   end
 end
--- spec/lib/blockchain_spec.rb
+++ spec/lib/blockchain_spec.rb
@@ -2,54 +2,8 @@ require 'rails_helper'
 require 'digest/sha2'
 
 describe Blockchain do
-  let(:blockchain) { Blockchain.new }
-
-  describe '#new' do
-    subject{ blockchain.last_block }
-    it 'appends a genesis block' do
-      expect(subject.index).to eq (0)
-      expect(subject.current_transactions).to be_empty
-      expect(subject.proof).to eq 100
-      expect(subject.previous_hash).to eq 1
-    end
-  end
-
-  describe '#append_transaction' do
-    subject(:index){ blockchain.append_transaction(sender: sender, recipient: recipient, amount: amount) }
-    subject(:transactions){ blockchain.current_transactions }
-
-    let(:sender) { 'sender1111' }
-    let(:recipient) { 'recipient2222' }
-    let(:amount) { 100 }
-
-    it 'returns current index' do
-      expect(index).to eq 0
-    end
-
-    it 'appends a new transaction' do
-      expect{ index }.to change{ transactions.length }.from(0).to(1)
-    end
-  end
-
-  describe '#append_block' do
-    subject(:block){ blockchain.append_block(proof: proof, previous_hash: previous_hash) }
-    subject(:chain){ blockchain.chain }
-
-    let(:proof) { 12345 }
-    let(:previous_hash) { 2 }
-
-    it 'returns a new block' do
-      expect(block.proof).to eq 12345
-      expect(block.previous_hash).to eq 2
-    end
-
-    it 'appends a new block' do
-      expect{ block }.to change{ chain.length }.from(1).to(2)
-    end
-  end
-
   describe '#compute_proof_of_work' do
-    subject(:proof_of_work){ blockchain.compute_proof_of_work(last_proof) }
+    subject(:proof_of_work){ Blockchain.compute_proof_of_work(last_proof) }
     subject(:result_hash) { Digest::SHA256.hexdigest(last_proof.to_s + proof_of_work.to_s) }
 
     let(:last_proof) { 111 }
