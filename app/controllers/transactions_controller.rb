b11bba55c05a795f6f1dcec7e34716eaa683ceaa
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-23 20:17:00 +0900

    add check_required_create_params before action to transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,22 @@
 class TransactionsController < ApplicationController
+
+  before_action :check_required_create_params
+
   def create
-    flash[:notice] = '新しいトランザクションを追加します'
+    flash[:notice] = 'Added a new transaction!'
     redirect_to blockchain_path
   end
+
+  private
+
+  def check_required_create_params
+    [:sender, :recipient, :amount].each do |p|
+      if params[p].blank?
+        flash[:error] = "You need to provide #{p} parameter."
+        redirect_to blockchain_path
+        break
+      end
+    end
+  end
+
 end
--- spec/app/controllers/transactions_controller_spec.rb
+++ spec/app/controllers/transactions_controller_spec.rb
@@ -4,15 +4,45 @@ describe TransactionsController, type: :controller do
   describe 'POST #create' do
 
     before do
-      post :create
+      post :create, params: { sender: sender, recipient: recipient, amount: amount }
     end
 
-    it 'redirects_to blockchain_path' do
-      expect(response).to redirect_to blockchain_path
+    describe '#check_required_create_params' do
+      context 'when providing all the necessary params' do
+        let(:sender) { 'test_sender' }
+        let(:recipient) { 'test_recipient' }
+        let(:amount) { 100 }
+
+        it 'redirects_to blockchain_path' do
+          expect(response).to redirect_to blockchain_path
+        end
+
+        it 'has the expected message' do
+          expect(flash[:error]).to_not eq 'You need to provide amount parameter.'
+        end
+      end
+
+      context 'when not providing all the necessary params' do
+        let(:sender) { 'test_sender' }
+        let(:recipient) { 'test_recipient' }
+        let(:amount) { nil }
+
+        it 'redirects_to blockchain_path' do
+          expect(response).to redirect_to blockchain_path
+        end
+
+        it 'has the expected message' do
+          expect(flash[:error]).to eq 'You need to provide amount parameter.'
+        end
+      end
     end
 
-    it 'has the expected message' do
-      expect(flash[:notice]).to eq '新しいトランザクションを追加します'
-    end
+    # it 'redirects_to blockchain_path' do
+    #   expect(response).to redirect_to blockchain_path
+    # end
+    #
+    # it 'has the expected message' do
+    #   expect(flash[:notice]).to eq '新しいトランザクションを追加します'
+    # end
   end
 end
